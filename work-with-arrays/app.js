4ff9220094ce9ef930af6be75a01717a3219bf65
Benjamin <benjamin.konopka@icloud.com>
2018-05-01 19:37:01 +0200

    Adding Example Code for filter() and split() function.

--- work-with-arrays/app.js
+++ work-with-arrays/app.js
@@ -1,3 +1,8 @@
+// 1) filter()
+// 2) map()
+
+
+
 const people = ['Beck, Glenn', 'Becker, Carl', 'Becket, Samuel', 'Beddoes, Mick'];
 
 const investors = [
@@ -11,15 +16,53 @@ const investors = [
     {first: 'Daniel', last: 'Düsentrieb', year: 1267, passed: 1301 },
 ]
 
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were borin in the 1500's
+// Working with Arrays
 
-// old way
-const fifteen = investors.filter(function(inventor) {
-    if(inventor.year >= 1500 && inventor.year < 1600) {
-        return true;
-    } else {
-        return false;
-    }
-});
-console.log(fifteen);
+// Array.prototype.filter()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// 1. Filter the list of inventors for those who were born in in the 1500's
+
+// Basic Way is a for Loop like this:
+const investorsBornIn1500 = []
+for (let i = 0; i < investors.length; i++) {
+    if (investors[i].year >= 1500 && investors[i].year < 1600)
+    investorsBornIn1500.push(investors[i])
+}
+console.table(investorsBornIn1500);
+// The Output is a new Array that contains Galilei and Kepler.
+
+// Same Way with the filter() function 
+const investorsInFifteen = investors.filter(function(inventor) {
+    return inventor.year >= 1500 && inventor.year < 1600
+})
+console.table(investorsInFifteen)
+// the Output Array has the same Content, but i think it looks a bit cleaner.
+
+// Lets check the ES6 (?) Way.
+const fifteen = investors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600))
+console.table(fifteen)
+// the Output Array has the same Content, but in ES6 - cool :) 
+
+// Play around with functions and Arrays
+// const checkforFifteen = function(i) {
+//   if(i.year >= 1500 && i.year < 1600){
+//       return true
+//   }
+// }
+// const fifteen = investors.filter(checkforFifteen);
+
+// Array.prototype.map()
+
+
+
+
+// .split() Examples
+let stringToSplit = 'Alle, meine Entchen';
+// console.log(stringToSplit.split(' '));
+// Output: (3) ["Alle,", "meine", "Entchen"]
+let stringToSplit1 = 'benny';
+// console.log(stringToSplit1.split(''));
+// Output: (5) ["b", "e", "n", "n", "y"]
+let stringToSplit2 = '3:45';
+// console.log(stringToSplit2.split(':'));
+// Output: (2) ["3", "45"]
